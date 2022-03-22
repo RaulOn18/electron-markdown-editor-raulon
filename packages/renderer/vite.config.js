fa822f3f61e210ba7c8dce73d46e21620eb45077
Radja Fajrul Ghufron <radjaoke18@gmail.com>
2022-03-23 05:40:50 +0700

    configure builinModules vite

--- packages/renderer/vite.config.js
+++ packages/renderer/vite.config.js
@@ -1,10 +1,10 @@
 /* eslint-env node */
 
-import {chrome} from '../../.electron-vendors.cache.json';
-import {join} from 'path';
-import {builtinModules} from 'module';
+import { chrome } from "../../.electron-vendors.cache.json"
+import { join } from "path"
+import { builtinModules } from "module"
 
-const PACKAGE_ROOT = __dirname;
+const PACKAGE_ROOT = __dirname
 
 /**
  * @type {import('vite').UserConfig}
@@ -15,33 +15,33 @@ const config = {
   root: PACKAGE_ROOT,
   resolve: {
     alias: {
-      '/@/': join(PACKAGE_ROOT, 'src') + '/',
-    },
+      "/@/": join(PACKAGE_ROOT, "src") + "/"
+    }
   },
   plugins: [],
-  base: '',
+  base: "",
   server: {
     fs: {
-      strict: true,
-    },
+      strict: true
+    }
   },
   build: {
     sourcemap: true,
     target: `chrome${chrome}`,
-    outDir: 'dist',
-    assetsDir: '.',
+    outDir: "dist",
+    assetsDir: ".",
     rollupOptions: {
-      input: join(PACKAGE_ROOT, 'index.html'),
+      input: join(PACKAGE_ROOT, "index.html"),
       external: [
-        ...builtinModules.flatMap(p => [p, `node:${p}`]),
-      ],
+        ...builtinModules.filter(m => (m != "process") & (m != "assert"))
+      ]
     },
     emptyOutDir: true,
-    brotliSize: false,
+    brotliSize: false
   },
   test: {
-    environment: 'happy-dom',
-  },
-};
+    environment: "happy-dom"
+  }
+}
 
-export default config;
+export default config
