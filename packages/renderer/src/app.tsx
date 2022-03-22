810ec0f30525b7623b1007e0428362e3c49ea44b
Radja Fajrul Ghufron <radjaoke18@gmail.com>
2022-03-22 16:29:24 +0700

    make an editor component

---
+++ packages/renderer/src/app.tsx
@@ -0,0 +1,17 @@
+import React, {useCallback, useState} from 'react'
+import Editor from './editor'
+import './app.css'
+
+const App: React.FC = () => {
+  const [doc, setDoc] = useState<string>("# Hello, World!\n")
+  const handleDocChange = useCallback(newDoc => {
+    setDoc(newDoc)
+  }, [])
+  return (
+    <div className='app'>
+      <Editor onChange={handleDocChange} initialDoc={doc} />
+    </div>
+  )
+}
+
+export default App
